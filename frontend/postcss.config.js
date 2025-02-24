a617ceb2417205c0aa8aec6a693607ee78e08e32
Kelvin <kelvinmwendadoreen@gmail.com>
2025-02-24 20:07:12 +0300

    tailwindcss challenges...

---
+++ frontend/postcss.config.js
@@ -0,0 +1,7 @@
+// filepath: /C:/Users/kelvi/OneDrive/Desktop/Recipe/recipe/frontend/postcss.config.js
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,5 +3,8 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
